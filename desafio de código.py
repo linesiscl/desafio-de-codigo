b9abf15e5afa55904fc973634755cf0e3b672192
Aline Fernandes Zeppelini <alinezeppelini@gmail.com>
2024-02-15 15:12:57 -0300

    casos de outros países

--- desafio de código.py
+++ desafio de código.py
@@ -16,6 +16,26 @@ def extrair_complicado(string_entrada):
 
     return nome, numero
 
+def extrair_outros(string_entrada):
+    partes = string_entrada.split()
+    numero = partes[0]
+    nome = ' '.join(partes[1:])
+
+    if numero.endswith(','):
+        numero = numero[:-1]
+
+    return nome, numero
+
+def extrair_outros2(string_entrada):
+    partes = string_entrada.split()
+    nome = ' '.join(partes[:2])
+    numero = ' '.join(partes[2:])
+
+    if nome.endswith(','):
+        nome = nome[:-1]
+
+    return nome, numero
+
 caso_simples = [
     "Miritiba 339",
     "Babaçu 500",
@@ -33,4 +53,22 @@ casos_complicados = [
 
 for endereco in casos_complicados:
     nome, numero = extrair_complicado(endereco)
+    print(f"{endereco} -> {nome}, {numero}")
+
+casos_complexos = [
+    "4, Rue de la République",
+    "100 Broadway Av"
+]
+
+for endereco in casos_complexos:
+    nome, numero = extrair_outros(endereco)
+    print(f"{endereco} -> {nome}, {numero}")
+
+casos_complexos2 = [
+    "Calle Sagasta, 26",
+    "Calle 44 No 1991"
+]
+
+for endereco in casos_complexos2:
+    nome, numero = extrair_outros2(endereco)
     print(f"{endereco} -> {nome}, {numero}")
